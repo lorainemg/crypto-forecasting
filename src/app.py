f94d0c8b6d5c75c701640a553e53fc174edc3756
Loraine Monteagudo <loraine.monteagudo@gmail.com>
2022-04-02 18:44:57 -0400

    Showing graphics

--- src/app.py
+++ src/app.py
@@ -1,16 +1,22 @@
 import streamlit as st
 from extract_data import get_coins, get_data
-
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.title('Cryptocurrency App')
 
 with st.sidebar:
     _, coin_symb, coin_ids = get_coins()
     coin_symb = st.selectbox('Select a coin', coin_symb)
-    coin_id = coin_ids[coin_symb.index(coin_symb)]
     days = st.number_input('Number of days to get the data', min_value=1, value=1)
     
     show_data = st.button('Show Data')
     
 if show_data:
-    data = get_data(coin_id, days)
+    data = get_data(coin_symb, days)
+    fig1 = px.line(data, x='dates', y='market_caps', title='Market Cap')
+    fig2 = px.line(data, x='dates', y='prices', title='Price')
+    fig3 = px.line(data, x='dates', y='total_volumes', title='Total Volume')
+    st.plotly_chart(fig1)
+    st.plotly_chart(fig2)
+    st.plotly_chart(fig3)
