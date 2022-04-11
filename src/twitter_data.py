f389ea1858b38fc45de2b29be946d55e648ae42e
Loraine Monteagudo <loraine.monteagudo@gmail.com>
2022-04-11 13:24:42 -0400

    Interface to extract twitter data ready

---
+++ src/twitter_data.py
@@ -0,0 +1,57 @@
+import datetime
+from typing import List
+import tweepy
+
+# Twitter API credentials (can be found in https://developer.twitter.com/en/portal/projects-and-apps)
+client = tweepy.Client(
+    consumer_key="API / Consumer Key here", 
+    consumer_secret="API / Consumer Secret here",
+    access_token="Access Token here",
+    access_token_secret="Access Token Secret here"
+)
+
+def get_tweets(cryptocurrency: str, start_time: datetime, end_time: datetime, max_results: int) -> List[dict]:
+    '''
+    Get tweets related to a specific cryptocurrency.
+    
+    Example of the response:
+    {
+        "data": [{
+            "id": "1373001119480344583",
+            "text": "Looking to get started with the Twitter API but new to APIs in general? @jessicagarson will walk you through everything you need to know in APIs 101 session."
+            "created_at": "2020-12-18T17:09:57.000Z"
+        }],
+        "meta": {
+            "newest_id": "1373001119480344583",
+            "oldest_id": "1364275610764201984",
+            "result_count": 6
+        }
+    }
+    '''
+    tweets = client.search_all_tweets(query=cryptocurrency, lang='en', max_results=max_results, 
+                                      start_time=start_time, end_time=end_time,
+                                      tweet_fields=['id', 'text', 'created_at'])
+    return tweets.json()
+
+
+def get_recent_tweets(cryptocurrency: str, max_results: int):
+    ''''
+    Get recent tweets related to a specific cryptocurrency.
+    
+    Example of the response:
+    {
+        "data": [{
+            "id": "1373001119480344583",
+            "text": "Looking to get started with the Twitter API but new to APIs in general? @jessicagarson will walk you through everything you need to know in APIs 101 session."
+            "created_at": "2020-12-18T17:09:57.000Z"
+        }],
+        "meta": {
+            "newest_id": "1373001119480344583",
+            "oldest_id": "1364275610764201984",
+            "result_count": 6
+        }
+    }
+    '''
+    tweets = client.search_recent_tweets(query=cryptocurrency, lang='en', max_results=max_results, 
+                                      tweet_fields=['id', 'text', 'created_at'])
+    return tweets.json()
