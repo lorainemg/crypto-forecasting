b9495b60d44fbf22796d6a5f823f3f34bdc738e4
Loraine Monteagudo <loraine.monteagudo@gmail.com>
2022-04-22 13:37:03 -0400

    Saving changes in pipfiles

--- src/twitter_data.py
+++ src/twitter_data.py
@@ -2,7 +2,7 @@ import datetime
 from pprint import pprint
 from typing import List
 import tweepy
-from config import TWITTER_API_KEY, TWITTER_API_KEY_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET, TWITTER_BEARER_TOKEN
+from config import *
 
 # Twitter API credentials (can be found in https://developer.twitter.com/en/portal/projects-and-apps)
 client = tweepy.Client(
@@ -13,10 +13,11 @@ client = tweepy.Client(
     bearer_token=TWITTER_BEARER_TOKEN
 )
 
-def get_tweets(cryptocurrency: str, start_time: datetime, end_time: datetime, max_results: int) -> List[tweepy.Tweet]:
+
+def get_tweets(cryptocurrency: str, max_results: int, start_time: datetime = None, end_time: datetime = None) -> List[tweepy.Tweet]:
     'Get tweets related to a specific cryptocurrency.'
     tweets = client.search_all_tweets(query=cryptocurrency, max_results=max_results, 
-                                    #   start_time=start_time, end_time=end_time,
+                                      start_time=start_time, end_time=end_time,
                                       tweet_fields=['id', 'text', 'created_at', 'lang'])
     return tweets.data
 
