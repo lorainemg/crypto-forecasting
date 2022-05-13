c458f5eb1aae9ccb24ecf341163681cba29a411f
Loraine Monteagudo <loraine.monteagudo@gmail.com>
2022-05-12 20:15:26 -0400

    Define all preprocessing functions

--- src/tweets/preprocess_tweets.py
+++ src/tweets/preprocess_tweets.py
@@ -10,6 +10,30 @@ class TweetsPreprocessing:
         'Removes the url of a list of tweets'
         return tweets.apply(lambda x: re.sub(r'https?://[^ ]+', '', x.text))
     
+    def remove_usernames(tweets: pd.DataFrame) -> pd.DataFrame:
+        'Removes the usernames of a list of tweets'
+        return tweets.apply(lambda x: re.sub(r'@[^ ]+', '', x.text))
+    
+    def deal_with_hashtags(tweets: pd.DataFrame) -> pd.DataFrame:
+        'Removes the character # to deal with hashtags'
+        return tweets.apply(lambda x: re.sub(r'#', '', x.text))
+
+    def character_normalization(tweets: pd.DataFrame) -> pd.DataFrame:
+        'Fix some uregurarly written words'
+        # removes letters written more than one time (too simple)
+        return tweets.apply(lambda x: re.sub(r'([A-Za-z])\1{2,}', r'\1', x.text))
+
+    def remove_special_characters(tweets: pd.DataFrame) -> pd.DataFrame:
+        'Remove punctuation and single characters'
+        def sub_special_char(tweet_text):
+            tweet_text = re.sub(r' 0 ', 'zero', tweet_text)
+            return re.sub(r'[^A-Za-z ]', '', tweet_text)
+        return tweets.apply(lambda x: sub_special_char(x.text))
+
+    def to_lower_case(tweets: pd.DataFrame) -> pd.DataFrame:
+        'Transforms all capital letters to their lower case equivalent'
+        return tweets.apply(lambda x: x.text.lower())
+
 
 if __name__ == '__main__':
     pass
